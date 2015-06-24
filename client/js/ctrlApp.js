4c1ffdc3a5db523dcec3c60eb772cba0c2703b08
nishi <nrg13736@nifty.com>
2015-06-24 07:39:22 +0000

    Chat Modifi

--- client/js/ctrlApp.js
+++ client/js/ctrlApp.js
@@ -9,40 +9,8 @@
 	app.controller('AppCtrl'
 				, ['$rootScope', '$scope'
 				, function($rootScope, $scope) {
-		
-		$scope.test = "";
-		
-        var socket = io.connect();
 
-        $scope.messages = [];
-        $scope.roster = [];
-        $scope.name = '';
-        $scope.text = '';
-
-        socket.on('connect', function () {
-          $scope.setName();
-        });
-
-        socket.on('message', function (msg) {
-          $scope.messages.push(msg);
-          $scope.$apply();
-        });
-
-        socket.on('roster', function (names) {
-          $scope.roster = names;
-          $scope.$apply();
-        });
-
-        $scope.send = function send() {
-          console.log('Sending message:', $scope.text);
-          socket.emit('message', $scope.text);
-          $scope.text = '';
-        };
-
-        $scope.setName = function setName() {
-          socket.emit('identify', $scope.name);
-        };
-        
+        // 日付編集		
         $scope.editDate = function editDate(datetime) {
           var m = moment(datetime);
           var formatDate = m.format('YYYY/MM/DD HH:mm:ss.SSS');
