5b4dd728d6d3d328c7336e36f364181123bf4353
nishi <nrg13736@nifty.com>
2015-06-25 00:04:43 +0000

    edit

--- client/js/ctrlApp.js
+++ client/js/ctrlApp.js
@@ -1,14 +1,47 @@
 	// ---------------------------------
 	// モジュール
 	// ---------------------------------
-	var app = angular.module('myApp', []);
+	var app = angular.module('myApp', ['ngRoute']);
 
 	// ---------------------------------
-	// コントローラー　PageCtrl
+	// Config
+	// ---------------------------------
+    app.config (['$stateProvider',function($stateProvider) {
+      $stateProvider
+        .state('main', {
+            url: '/aa',
+            controller: 'AppCtrl',
+            templateUrl: 'main.html'
+        })
+        .state('settings', {
+            url: '/settings',
+            controller: 'SettingsCtrl',
+            templateUrl: 'settings.html'
+        })
+        .state('settings.profile', {
+            url: '/account',
+            controller: 'SettingsCtrl',
+            templateUrl: 'settings/profile.html'
+        })
+    }]);
+
+	// ---------------------------------
+	// コントローラー　AppCtrl
 	// ---------------------------------
 	app.controller('AppCtrl'
 				, ['$rootScope', '$scope'
 				, function($rootScope, $scope) {
+
+        // // テンプレート
+        // $scope.templates = [
+        //                         { name: 'template1.html', url: 'template1.html'},
+        //                         { name: 'template2.html', url: 'template2.html'},
+        //                     ];
+        // $scope.template = $scope.templates[1];
+
+        $scope.init = function init() {
+          $scope.$apply();
+        };
 
         // 日付編集		
         $scope.editDate = function editDate(datetime) {
--- client/js/ctrlChat.js
+++ client/js/ctrlChat.js
@@ -1,37 +1,44 @@
-app.controller('ChatCtrl', function($scope, $controller) {
-    
-    // AppCtrlから継承
-    $controller('AppCtrl', {$scope: $scope});
-
-        var socket = io.connect();
-
-        $scope.messages = [];
-        $scope.roster = [];
-        $scope.name = '';
-        $scope.text = '';
-
-        socket.on('connect', function () {
-          $scope.setName();
-        });
-
-        socket.on('message', function (msg) {
-          $scope.messages.push(msg);
-          $scope.$apply();
-        });
-
-        socket.on('roster', function (names) {
-          $scope.roster = names;
-          $scope.$apply();
-        });
-
-        $scope.send = function send() {
-          console.log('Sending message:', $scope.text);
-          socket.emit('message', $scope.text);
-          $scope.text = '';
-        };
-
-        $scope.setName = function setName() {
-          socket.emit('identify', $scope.name);
-        };
+	// ---------------------------------
+	// コントローラー　ChatCtrl
+	// ---------------------------------
+    app.controller('ChatCtrl', function($scope, $controller) {
         
-});
+        // AppCtrlから継承
+        $controller('AppCtrl', {$scope: $scope});
+    
+            var socket = io.connect();
+    
+            $scope.messages = [];
+            $scope.roster = [];
+            $scope.name = '';
+            $scope.text = '';
+    
+            socket.on('connect', function () {
+              $scope.setName();
+            });
+    
+            socket.on('message', function (msg) {
+              $scope.messages.push(msg);
+              $scope.$apply();
+            });
+    
+            socket.on('roster', function (names) {
+              $scope.roster = names;
+              $scope.$apply();
+            });
+    
+            $scope.init = function init() {
+              $scope.$apply();
+            };
+    
+            $scope.send = function send() {
+              console.log('Sending message:', $scope.text);
+              socket.emit('message', $scope.text);
+              $scope.text = '';
+            };
+    
+            $scope.setName = function setName() {
+              socket.emit('identify', $scope.name);
+            };
+            
+    });
