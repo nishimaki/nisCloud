0dec5f3a242d9c100f6dc061d60d450736f7284c
nishi <nrg13736@nifty.com>
2015-07-02 04:01:11 +0000

    update

--- client/js/ctrlMaintenance.js
+++ client/js/ctrlMaintenance.js
@@ -9,7 +9,27 @@
         $controller('AppCtrl', {$rootScope: $rootScope, $scope: $scope});
     
         $scope.init = function init() {
-          console.log("MaintenanceCtrl init");
+            console.log("MaintenanceCtrl init");
+          
+          
+            var data = [
+                ["", "Ford", "Volvo", "Toyota", "Honda"],
+                ["2014", 10, 11, 12, 13],
+                ["2015", 20, 11, 14, 13],
+                ["2016", 30, 15, 12, 13]
+            ];
+            
+            var container = document.getElementById('example');
+            var hot = new Handsontable(container, {
+                data: data,
+                minSpareRows: 1,
+                rowHeaders: true,
+                colHeaders: true,
+                contextMenu: true
+            });
+
+          
+          
         };
 
     });
